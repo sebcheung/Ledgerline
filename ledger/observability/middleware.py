9eb1318e72c785afce4dbf99d42b7b36a4a5c555
Sebastian Cheung <sebcheung64@gmail.com>
2026-08-28 15:56:22 -0400

    Add structured JSON logging and request middleware

---
+++ ledger/observability/middleware.py
@@ -0,0 +1,21 @@
+import uuid
+from collections.abc import Awaitable, Callable
+
+import structlog
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+
+
+class RequestIdMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    ) -> Response:
+        request_id = request.headers.get("x-request-id", str(uuid.uuid4()))
+        structlog.contextvars.bind_contextvars(request_id=request_id)
+        try:
+            response = await call_next(request)
+        finally:
+            structlog.contextvars.clear_contextvars()
+        response.headers["x-request-id"] = request_id
+        return response
